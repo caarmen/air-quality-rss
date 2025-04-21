2254c830eb037db44091f01ed5039d437d0e3d54
Carmen Alvarez <carmen@rmen.ca>
2025-04-21 16:22:30 +0200

    The api may return a larger string for "responsible pollens".

--- src/pollen/service/pollen-parser.cob
+++ src/pollen/service/pollen-parser.cob
@@ -22,7 +22,7 @@
        FILE SECTION.
        FD POLLEN-FILE.
        01 DATE-MAJ                    PIC X(24).
-       01 RESPONSIBLE-POLLEN           PIC X(16) VALUE SPACES.
+       01 RESPONSIBLE-POLLEN           PIC X(64) VALUE SPACES.
        01 POLLEN-RECORD.
            05 POLLEN-NAME               PIC X(16).
            05 POLLEN-CODE               PIC 9(1).
