c704310ad1276a8152b3b85c36dea6418e8aa1c1
Carmen Alvarez <carmen@rmen.ca>
2025-04-23 23:12:52 +0200

    Handle a generic cjson error.
    Add a check on the json error pointer after each cjson api call.

--- src/pollen/service/pollen-parser.cob
+++ src/pollen/service/pollen-parser.cob
@@ -23,6 +23,8 @@
        COPY pollen-data IN "pollen/service".
 
        LOCAL-STORAGE SECTION.
+       01 JSON-ERROR-PTR               USAGE POINTER.
+       01 JSON-ERROR-MSG               PIC X(10000).
        01 JSON-ROOT-PTR                USAGE POINTER.
        01 JSON-FEATURES-PTR            USAGE POINTER.
        01 JSON-FIRST-FEATURE-PTR       USAGE POINTER.
@@ -75,18 +77,21 @@
            CALL "cJSON_Parse" USING
                BY CONTENT FUNCTION TRIM(POLLEN-JSON-INPUT)
                RETURNING JSON-ROOT-PTR
+           PERFORM CHECK-JSON-ERROR
 
       *> Get the "features" attribute, which is an array:
            CALL "JSON-GET-OBJECT" USING
                BY CONTENT FEATURES-ATTRIBUTE
                BY VALUE JSON-ROOT-PTR
                BY REFERENCE JSON-FEATURES-PTR
+           PERFORM CHECK-JSON-ERROR
 
       *> Get the first feature (there's only ever one it seems).
            CALL "cJSON_GetArrayItem" USING
                BY VALUE JSON-FEATURES-PTR
                0
                RETURNING JSON-FIRST-FEATURE-PTR
+           PERFORM CHECK-JSON-ERROR
 
            IF JSON-FIRST-FEATURE-PTR = NULL
            THEN
@@ -100,6 +105,7 @@
                BY VALUE JSON-FIRST-FEATURE-PTR
                BY CONTENT PROPERTIES-ATTRIBUTE
                RETURNING JSON-PROPERTIES-PTR
+           PERFORM CHECK-JSON-ERROR
 
            IF JSON-PROPERTIES-PTR NOT = NULL
            THEN
@@ -113,6 +119,7 @@
                    BY VALUE JSON-PROPERTIES-PTR
                    BY REFERENCE DATE-MAJ-ATTRIBUTE
                    BY REFERENCE DATE-MAJ
+               PERFORM CHECK-JSON-ERROR
                WRITE DATE-MAJ
 
                *> Get the "pollen_resp" attribute, which is a
@@ -123,11 +130,13 @@
                    BY VALUE JSON-PROPERTIES-PTR
                    BY REFERENCE POLLEN-RESP-ATTRIBUTE
                    BY REFERENCE RESPONSIBLE-POLLEN
+               PERFORM CHECK-JSON-ERROR
                WRITE RESPONSIBLE-POLLEN
 
                CALL "cJSON_GetArraySize" USING
                    BY VALUE JSON-PROPERTIES-PTR
                    RETURNING JSON-PROPERTIES-SIZE
+               PERFORM CHECK-JSON-ERROR
 
                *> Iterate over all the properties, looking for
                *> the ones prefixed with code_. These are the pollen
@@ -142,11 +151,13 @@
                            BY VALUE JSON-PROPERTIES-PTR
                            PROPERTY-ATTR-INDEX
                            RETURNING PROPERTY-ATTR-PTR
+                       PERFORM CHECK-JSON-ERROR
 
                        *> PROPERTY-NAME-VAL will be like "code_boul"
                        CALL "JSON-GET-OBJECT-NAME" USING
                            BY VALUE PROPERTY-ATTR-PTR
                            BY REFERENCE PROPERTY-NAME-VAL
+                       PERFORM CHECK-JSON-ERROR
 
                        *> Ignore code_qual and code_zone which
                        *> aren't pollen codes. All other code_
@@ -162,6 +173,7 @@
                            CALL "cJSON_GetIntValue" USING
                                BY VALUE PROPERTY-ATTR-PTR
                                RETURNING POLLEN-CODE
+                       PERFORM CHECK-JSON-ERROR
                            WRITE POLLEN-RECORD
                        END-IF
                END-PERFORM
@@ -169,5 +181,29 @@
            END-IF
            MOVE 0 TO RETURN-CODE
            GOBACK.
+
+      *> ===============================================================
+      *> PARAGRAPH: CHECK-JSON-ERROR
+      *> PURPOSE: Check if the cJSON library has "raised an error".
+      *>          This is done by setting the JSON-ERROR-PTR to a
+      *>          position in the json input where the error starts.
+      *>          If this happens:
+      *>            - log the error information,
+      *>            - set an error RETURN-CODE,
+      *>            - Stop execution of the POLLEN-PARSER program.
+      *> ===============================================================
+       CHECK-JSON-ERROR.
+           CALL "cJSON_GetErrorPtr"
+               RETURNING JSON-ERROR-PTR
+           IF JSON-ERROR-PTR NOT = NULL
+           THEN
+               DISPLAY "Json error occurred"
+               CALL "C-STRING" USING
+                   BY VALUE     JSON-ERROR-PTR
+                   BY REFERENCE JSON-ERROR-MSG
+               DISPLAY "Json error ptr: " FUNCTION TRIM (JSON-ERROR-MSG)
+               MOVE 1 to RETURN-CODE
+               GOBACK
+           END-IF.
 
        END PROGRAM POLLEN-PARSER.
