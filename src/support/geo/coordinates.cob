38de85b58c4d6c9131aef4e17bb80015a1dc9e7f
Carmen Alvarez <carmen@rmen.ca>
2025-04-27 22:22:34 +0200

    Prefix `LINKAGE` data items with `IN-` or `OUT-` (coodinates.cob).

--- src/support/geo/coordinates.cob
+++ src/support/geo/coordinates.cob
@@ -19,24 +19,24 @@
            01  C-EARTH-RADIUS               CONSTANT AS 6378137.
 
        LINKAGE SECTION.
-           01  LATITUDE                     PIC S9(3)V9(8).
-           01  LONGITUDE                    PIC S9(3)V9(8).
-           01  X                            PIC S9(7)V9(8).
-           01  Y                            PIC S9(7)V9(8).
+           01  IN-LATITUDE                  PIC S9(3)V9(8).
+           01  IN-LONGITUDE                 PIC S9(3)V9(8).
+           01  OUT-X                        PIC S9(7)V9(8).
+           01  OUT-Y                        PIC S9(7)V9(8).
 
        PROCEDURE DIVISION USING
-           BY REFERENCE LATITUDE
-           BY REFERENCE LONGITUDE
-           BY REFERENCE X
-           BY REFERENCE Y.
+           BY REFERENCE IN-LATITUDE
+           BY REFERENCE IN-LONGITUDE
+           BY REFERENCE OUT-X
+           BY REFERENCE OUT-Y.
 
            *> Convert longitude to X coordinate
-           COMPUTE X = C-EARTH-RADIUS * LONGITUDE * C-PI / 180.
+           COMPUTE OUT-X = C-EARTH-RADIUS * IN-LONGITUDE * C-PI / 180.
 
            *> Convert latitude to Y coordinate using Mercator formula
-           COMPUTE Y = C-EARTH-RADIUS *
+           COMPUTE OUT-Y = C-EARTH-RADIUS *
                FUNCTION LOG(FUNCTION TAN(
-                   ((LATITUDE * C-PI / 180) / 2) + (C-PI / 4))).
+                   ((IN-LATITUDE * C-PI / 180) / 2) + (C-PI / 4))).
 
            GOBACK.
        END PROGRAM LAT-LONG-TO-WEB-MERCATOR.
