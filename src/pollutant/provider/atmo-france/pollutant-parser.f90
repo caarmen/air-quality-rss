9820b87fd2f012a2a3c46439f21131f353af37ef
Carmen Alvarez <carmen@rmen.ca>
2025-08-10 19:30:49 +0200

    Prepare parsing the atmo france pollutant data from the admin api.
    Extract the json parsing for the json content which is common to the admin api and tabular api, into a new routine `parse_pollutant_data_json_object`.

--- src/pollutant/provider/atmo-france/pollutant-parser.f90
+++ src/pollutant/provider/atmo-france/pollutant-parser.f90
@@ -5,7 +5,8 @@ module atmo_france_pollutant_parser
 
 contains
    !-----------------------------------------------------------------------
-   ! Subroutine to parse atmo france pollutant data
+   ! Subroutine to parse atmo france pollutant data from an atmo france
+   ! tabular api response json string.
    !
    ! Arguments:
    !
@@ -37,9 +38,6 @@ contains
       type(json_core) :: json_core_obj
       type(json_value), pointer :: json_root, json_pollutant_data
 
-      integer :: i, pollutant_index
-      logical :: found_pollutant
-
       ! pollutant_data_json_str:
       ! [
       !   {
@@ -62,11 +60,62 @@ contains
       !     "code_pm25": 1,
       !     "code_so2": 1
       !   }
-      call json_core_obj%print(json_pollutant_data)
+      call parse_pollutant_data_json_object( &
+         json_pollutant_data, &
+         pollutant_names, &
+         data, &
+         pollutant_count &
+         )
+
+      call json_response%destroy()
+
+   end subroutine parse_pollutant_data
+
+   !-----------------------------------------------------------------------
+   ! Subroutine to return pollutant data from an atmo france json object.
+   !
+   ! Arguments:
+   !
+   !   in  :: pollutant_data_json_obj  - The pollutant data as a json
+   !                                     object.
+   !                                     Example content:
+   !                                     {
+   !                                       "code_no2": 2,
+   !                                       "code_o3": 2,
+   !                                       "code_pm10": 1,
+   !                                       "code_pm25": 1,
+   !                                       "code_so2": 1
+   !                                     }
+   !   in  :: pollutant_names          - List of pollutant names for which
+   !                                     to fetch data.
+   !
+   !   out :: data                     - The parsed pollutant data.
+   !   out :: pollutant_count          - The number of pollutants in the
+   !                                     the parsed result.
+   !-----------------------------------------------------------------------
+   subroutine parse_pollutant_data_json_object( &
+      pollutant_data_json_obj, &
+      pollutant_names, &
+      data, &
+      pollutant_count &
+      )
+      use json_module, only: json_file, json_core, json_value
+      use atmo_france_pollutant_data, only: pollutant_data
+      implicit none
+      type(json_value), pointer :: pollutant_data_json_obj
+      character(len=4), intent(in) :: pollutant_names(:)
+      integer, intent(out) :: pollutant_count
+      type(pollutant_data), intent(out) ::  data(*)
+
+      type(json_core) :: json_core_obj
+      integer :: i, pollutant_index
+      logical :: found_pollutant
+
+      call json_core_obj%print(pollutant_data_json_obj)
       pollutant_count = 0
       do i = 1, size(pollutant_names)
          ! Look up a pollutant name like "o3". We look up "code_o3".
-         call json_core_obj%get(json_pollutant_data, "code_"//trim(pollutant_names(i)), pollutant_index, found_pollutant)
+         call json_core_obj%get(pollutant_data_json_obj, "code_"//trim(pollutant_names(i)), pollutant_index, found_pollutant)
          ! If the json has a value for "code_o3, increment the number of
          ! pollutants found and add an entry to our return data.
          if (found_pollutant) then
@@ -75,8 +124,5 @@ contains
             data(pollutant_count)%index = pollutant_index
          end if
       end do
-
-      call json_response%destroy()
-
-   end subroutine parse_pollutant_data
+   end subroutine parse_pollutant_data_json_object
 end module atmo_france_pollutant_parser
