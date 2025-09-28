3055f4e42df580fe888ce987992ecbe3b828e852
Carmen Alvarez <carmen@rmen.ca>
2025-09-28 11:05:47 +0200

    Add script to run the docker container in bash.

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker rm airqual
+docker run \
+   --name airqual \
+   -e BASE_FEED_URL=http://localhost:8888 \
+   -e POLLUTANT_METADATA_URL=http://host.docker.internal:9999/pollutant/response.json \
+   -e POLLEN_BASE_URL=http://host.docker.internal:9999/pollen/response.json \
+   -v $(pwd)/.env.sh:/app/.env.sh:ro \
+   -p 8888:8888 \
+   -it --entrypoint /bin/bash \
+   --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+   airqual:latest
+
+#   --memory=1000m \
+#   --memory-swap=1000m \
